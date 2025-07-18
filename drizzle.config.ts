b92adb7113db0fa6a9eccd3bab4cb57842e67eed
santr4 <santra.aaranyak16092003@gmail.com>
2025-07-18 10:59:37 +0530

    02: database

---
+++ drizzle.config.ts
@@ -0,0 +1,10 @@
+import "dotenv/config";
+
+export default {
+  out: './drizzle',
+  schema: './src/db/schema.ts',
+  dialect: 'postgresql',
+  dbCredentials: {
+    url: process.env.DATABASE_URL!,
+  },
+};
